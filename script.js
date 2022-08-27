955e2ea51520a636fe38cbf344ea5af7fb6f35ab
Vetri Suriya <34440110+vetrisuriya@users.noreply.github.com>
2022-08-27 21:06:36 +0530

    Add files via upload
    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let mobileNavBtn = document.getElementById("mobile-nav-btn");
+let mobileNav = document.getElementById("mobile-nav");
+
+mobileNavBtn.addEventListener("click", function() {
+    if(mobileNav.hasAttribute("class")) {
+        mobileNav.removeAttribute("class");
+    } else {
+        mobileNav.setAttribute("class", "active");
+    }
+})
